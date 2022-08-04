be5311b0381ea3f26d272c43ed3a07d12b6afdd2
Xiaoda11 <1123297903@qq.com>
2022-08-04 15:19:14 +0800

    master

--- src/remane_topic/scripts/topic_name.py
+++ src/remane_topic/scripts/topic_name.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python
 # -*-coding:utf-8-*-
-from tkinter import N
 import rospy
 from std_msgs.msg import String
 
 if __name__=="__main__":
     rospy.init_node("hello_ss")
+    """
+    实现不同类型的话题设置
+    
+    """
+    # 全局
+    pub=rospy.Publisher("/chatter",String,queue_size=10)
 
+    # 相对
+    pub=rospy.Publisher("chatter",String,queue_size=10)
 
+    # 私有
+    pub=rospy.Publisher("~chatter",String,queue_size=10)
     while not rospy.is_shutdown():
         pass
